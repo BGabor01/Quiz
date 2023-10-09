8729c83d49fe496b19ce5d7c396da1b034a0b60e
BGabor01 <busnes14@gmail.com>
2023-10-09 18:01:55 +0200

    Mapped answers to question

--- src/main/java/Gapps/quiz/models/QuestionModel.java
+++ src/main/java/Gapps/quiz/models/QuestionModel.java
@@ -1,20 +1,28 @@
 package Gapps.quiz.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
 @Data
 public class QuestionModel {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String question;
 
-    private String answer;
+    private Boolean isCorrect;
+
+    @OneToMany(mappedBy = "questionModel", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AnswerModel> answers = new ArrayList<>();
 
 }
