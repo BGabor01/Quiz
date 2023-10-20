7b2b521d3c42fc6df7aa807c1398db5878661271
BGabor01 <busnes14@gmail.com>
2023-10-20 22:58:42 +0200

    AnswerDto: Dto for every possible answer

--- src/main/java/Gapps/quiz/DTO/AnswerDto.java
+++ src/main/java/Gapps/quiz/DTO/AnswerDto.java
@@ -6,8 +6,8 @@ import lombok.Data;
 
 @Data
 public class AnswerDto {
-
     @NotBlank(message = "Answer text cannot be blank")
     private String text;
-    private boolean isCorrect;
+
+    private boolean correct;
 }
