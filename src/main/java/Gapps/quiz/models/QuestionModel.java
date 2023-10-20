1e3dd8ee8cb5f80e809b048145b01ab082d67a0d
BGabor01 <busnes14@gmail.com>
2023-10-20 23:02:08 +0200

    QuestionModel: Made relation to answer

--- src/main/java/Gapps/quiz/models/QuestionModel.java
+++ src/main/java/Gapps/quiz/models/QuestionModel.java
@@ -3,6 +3,8 @@ package Gapps.quiz.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,9 +22,7 @@ public class QuestionModel {
 
     private String question;
 
-    private Boolean isCorrect;
-
     @OneToMany(mappedBy = "questionModel", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
     private List<AnswerModel> answers = new ArrayList<>();
-
 }
